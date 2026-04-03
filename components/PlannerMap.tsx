0e41b3c3d30cc1dc70d593df28fedad5344aaa6c
Rocean <roceanadhikari@gmail.com>
2026-04-03 10:56:25 +0530

    planner fixed for proper orientation

--- components/PlannerMap.tsx
+++ components/PlannerMap.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Polyline, Popup, useMap } from 'react-leaflet';
 import type { RunRecord, LatLng } from '@/types';
 import { decodePolyline } from '@/lib/polyline';
